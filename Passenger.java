2de96f4d1699227147204652b497bc7d5184c776
Teddy Martin <tkmartin@smith.edu>
2023-03-07 19:46:12 -0500

    edited Passenger to add getOffCar and boardCar

--- Passenger.java
+++ Passenger.java
@@ -1,8 +1,30 @@
+// `public void boardCar(Car c)` can call `c.addPassenger(this)` to board a given `Car` 
+// (_Hint: this method should be ready to `catch` the `RuntimeException` that will be thrown by `c.addPassenger(...)` 
+// in the event that the car is full._)
+// `public void getOffCar(Car c)` can call `c.removePassenger(this)` to get off a given `Car` 
+// (_Hint: this method should be ready to `catch` the `RuntimeException` that will be thrown by `c.removePassenger(...)` in the event 
+// that the `Passenger` wasn't actually onboard._)
+
+
 public class Passenger {
     
     private String name;
 
     public Passenger(String name) {
         this.name = name;
+    }
+    public void boardCar(Car c) {
+        try {
+            c.addPassenger(this);
+        } catch (Exception e) {
+            System.out.println("Car is already full." + this + " cannot board this car.");
+    }
+
+    public void getOffCar(Car c) {
+        try {
+        c.removePassenger(this);
+        } catch (Exception e) {
+            System.out.println(this + " isn't onboard this car.");
+        }
     }
 }
