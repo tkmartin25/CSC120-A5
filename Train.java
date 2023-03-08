e7c0b13679d65afebf68db874ad49d64a4723b93
Teddy Martin <tkmartin@smith.edu>
2023-03-07 23:12:36 -0500

    started working on Train.java -- not finished

--- Train.java
+++ Train.java
@@ -1,3 +1,24 @@
+ // a private `Engine` attribute, which we will mark with the keyword `final` to establish the **composition** relationship 
+ // (e.g. `private final Engine engine;`)
+ // a private `ArrayList` to keep track of the `Car`s currently attached
+ // a constructor `public Train(FuelType fuelType, double fuelCapacity, int nCars, int passengerCapacity)`
+ // which will initialize the `Engine` and `Car`s and store them
+ // a couple of accessors: 
+    // `public Engine getEngine()`
+    // `public Car getCar(int i)` to return the `i`th car
+    // `public int getMaxCapacity()` which will return the maximum total capacity across all `Car`s
+    // `public int seatsRemaining()` which will return the number of remaining open seats across all `Car`s
+// and finally, its own `public void printManifest()` that prints a roster of all `Passenger`s onboard (_Hint: your `Car`s can help!_)
+import java.util.ArrayList;
+
 public class Train {
+    private final Enginge engine;
+    private ArrayList<Car> cars_attached;
+
+    public Train(FuelType fuelType, double fuelCapacity, int nCars, int passengerCapacity) {
+
+    }
+
+
 
 }
